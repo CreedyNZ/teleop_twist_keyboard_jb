5b435a4e750076ea1c51fa039e6859d1deba0e53
CreedyNZ <andrew@creahan.co.nz>
2015-10-09 10:59:50 +1300

    Add y values

--- teleop_twist_keyboard.py
+++ teleop_twist_keyboard.py
@@ -14,6 +14,9 @@ Moving around:
    j    k    l
    m    ,    .
 
+v : rotate left
+b ; rotate right
+
 q/z : increase/decrease max speeds by 10%
 w/x : increase/decrease only linear speed by 10%
 e/c : increase/decrease only angular speed by 10%
@@ -23,84 +26,93 @@ CTRL-C to quit
 """
 
 moveBindings = {
-		'i':(1,0),
-		'o':(1,-1),
-		'j':(0,1),
-		'l':(0,-1),
-		'u':(1,1),
-		',':(-1,0),
-		'.':(-1,1),
-		'm':(-1,-1),
-	       }
+        'i':(1,0),
+        'o':(1,-1),
+        'j':(0,1),
+        'l':(0,-1),
+        'u':(1,1),
+        ',':(-1,0),
+        '.':(-1,1),
+        'm':(-1,-1),
+           }
+
+rotateBindings = {
+        'v':(0,1),
+        'b':(0,-1),
+           }
 
 speedBindings={
-		'q':(1.1,1.1),
-		'z':(.9,.9),
-		'w':(1.1,1),
-		'x':(.9,1),
-		'e':(1,1.1),
-		'c':(1,.9),
-	      }
+        'q':(1.1,1.1),
+        'z':(.9,.9),
+        'w':(1.1,1),
+        'x':(.9,1),
+        'e':(1,1.1),
+        'c':(1,.9),
+          }
 
 def getKey():
-	tty.setraw(sys.stdin.fileno())
-	select.select([sys.stdin], [], [], 0)
-	key = sys.stdin.read(1)
-	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-	return key
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
 speed = .5
 turn = 1
 
 def vels(speed,turn):
-	return "currently:\tspeed %s\tturn %s " % (speed,turn)
+    return "currently:\tspeed %s\tturn %s " % (speed,turn)
 
 if __name__=="__main__":
-    	settings = termios.tcgetattr(sys.stdin)
-	
-	pub = rospy.Publisher('cmd_vel', Twist)
-	rospy.init_node('teleop_twist_keyboard')
+    settings = termios.tcgetattr(sys.stdin)
 
-	x = 0
-	th = 0
-	status = 0
+    pub = rospy.Publisher('cmd_vel', Twist)
+    rospy.init_node('teleop_twist_keyboard')
 
-	try:
-		print msg
-		print vels(speed,turn)
-		while(1):
-			key = getKey()
-			if key in moveBindings.keys():
-				x = moveBindings[key][0]
-				th = moveBindings[key][1]
-			elif key in speedBindings.keys():
-				speed = speed * speedBindings[key][0]
-				turn = turn * speedBindings[key][1]
+    x = 0
+    y =0
+    th = 0
+    status = 0
 
-				print vels(speed,turn)
-				if (status == 14):
-					print msg
-				status = (status + 1) % 15
-			else:
-				x = 0
-				th = 0
-				if (key == '\x03'):
-					break
+    try:
+        print msg
+        print vels(speed,turn)
+        while(1):
+            key = getKey()
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][1]
+            elif key in rotateBindings.keys():
+                th = rotateBindings[key][1]
+            elif key in speedBindings.keys():
+                speed = speed * speedBindings[key][0]
+                turn = turn * speedBindings[key][1]
 
-			twist = Twist()
-			twist.linear.x = x*speed; twist.linear.y = 0; twist.linear.z = 0
-			twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th*turn
-			pub.publish(twist)
+                print vels(speed,turn)
+                if (status == 14):
+                    print msg
+                status = (status + 1) % 15
+            else:
+                x = 0
+                y = 0
+                th = 0
+                if (key == '\x03'):
+                    break
 
-	except:
-		print e
+            twist = Twist()
+            twist.linear.x = x*speed; twist.linear.y = y*speed; twist.linear.z = 0
+            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th*turn
+            pub.publish(twist)
 
-	finally:
-		twist = Twist()
-		twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
-		twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
-		pub.publish(twist)
+    except:
+        print "Keyboard Error"
 
-    		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    finally:
+        twist = Twist()
+        twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
+        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        pub.publish(twist)
+
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
 
